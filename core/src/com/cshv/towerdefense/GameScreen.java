89f33fb2ea6d086c42b06a18d7065d3cf5e5d007
BarricadeX75 <pf.clement.saintmarc@gmail.com>
2018-03-17 02:48:04 -1000

    Commit

--- core/src/com/cshv/towerdefense/GameScreen.java
+++ core/src/com/cshv/towerdefense/GameScreen.java
@@ -4,4 +4,11 @@ import com.badlogic.gdx.ScreenAdapter;
 
 
 public class GameScreen extends ScreenAdapter {
+
+    private final TowerDefenseGame towerDefenseGame;
+
+
+    public GameScreen(TowerDefenseGame towerDefenseGame) {
+        this.towerDefenseGame = towerDefenseGame;
+    }
 }
--- core/src/com/cshv/towerdefense/StartScreen.java
+++ core/src/com/cshv/towerdefense/StartScreen.java
@@ -4,4 +4,11 @@ import com.badlogic.gdx.ScreenAdapter;
 
 
 public class StartScreen extends ScreenAdapter {
+
+    private final TowerDefenseGame towerDefenseGame;
+
+
+    public StartScreen(TowerDefenseGame towerDefenseGame) {
+        this.towerDefenseGame = towerDefenseGame;
+    }
 }
