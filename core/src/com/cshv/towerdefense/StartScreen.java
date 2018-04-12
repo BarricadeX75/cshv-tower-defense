88347dc7b5201bda0b7e40b8fcfd1ab7ac7f292e
BarricadeX75 <pf.clement.saintmarc@gmail.com>
2018-04-12 00:22:33 -1000

    Commit

--- core/src/com/cshv/towerdefense/StartScreen.java
+++ core/src/com/cshv/towerdefense/StartScreen.java
@@ -1,14 +1,60 @@
 package com.cshv.towerdefense;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 
 public class StartScreen extends ScreenAdapter {
+
+    private static final float WORLD_WIDTH = TowerDefenseGame.WORLD_WIDTH;
+    private static final float WORLD_HEIGHT = TowerDefenseGame.WORLD_HEIGHT;
+
+    private Stage stage;
 
     private final TowerDefenseGame towerDefenseGame;
 
 
     public StartScreen(TowerDefenseGame towerDefenseGame) {
         this.towerDefenseGame = towerDefenseGame;
+    }
+
+    @Override
+    public void show() {
+        super.show();
+        stage = new Stage(new FitViewport(WORLD_WIDTH, WORLD_HEIGHT));
+        Gdx.input.setInputProcessor(stage);
+
+        //TextureAtlas textureAtlas = towerDefenseGame.getAssetManager().get("");
+
+        //
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        clearScreen();
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        stage.dispose();
+    }
+
+    private void clearScreen() {
+        Gdx.gl.glClearColor(Color.BLACK.r, Color.BLACK.g, Color.BLACK.b, Color.BLACK.a);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 }
